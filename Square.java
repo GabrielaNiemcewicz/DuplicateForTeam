736af3a2808062ad8547ac9be36e251ae17b5bd6
GabrielaNiemcewicz <43764470+GabrielaNiemcewicz@users.noreply.github.com>
2020-04-02 23:07:40 +0200

    square chris

--- Square.java
+++ Square.java
@@ -1,97 +1,52 @@
-import javafx.geometry.Pos;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+public class Square {
 
-public class Square extends StackPane {
-	static boolean selected;
-	private Tile tile;
-	private Type type;
-	Rectangle r = new Rectangle(40, 40);
-	Text text = new Text();
+    private int letterMuliplier;
+    private int wordMultiplier;
+    private boolean isOccupied;
+    private Tile tile;
 
-	Square(){
-		setType(Type.NORMAL);
-		setStyle();
-	}
+    Square(int letterMultiplier, int wordMultiplier) {
+        isOccupied = false;
+        this.letterMuliplier = letterMultiplier;
+        this.wordMultiplier = wordMultiplier;
+    }
 
-	private void setStyle(){
-		r.setFill(type.getColour());
-		r.setStroke(Color.BLACK);
-		text.setFont(Font.font("verdana", FontWeight.BOLD, 14));
-		text.setFill(Color.WHITE);
-		setAlignment(Pos.CENTER);
-		getChildren().addAll(r, text);
-	}
+    public int getLetterMuliplier() {
+        return letterMuliplier;
+    }
 
-	public void setType(Type type) {
-		this.type = type;
-		r.setFill(type.getColour());
-		text.setText(type.toString());
-	}
-	public void placeTile(Tile tile){
-		if (this.isEmpty())	
-			this.tile = tile;
-		text.setText("" + tile.getLetter());
-		text.setFill(Color.BLACK);
-		r.setFill(Color.valueOf("FFE98D"));
-		r.setArcHeight(5);
-		r.setArcWidth(5);
+    public int getWordMultiplier() {
+        return wordMultiplier;
+    }
 
-	}
+    public boolean isDoubleLetter() {
+        return letterMuliplier == 2;
+    }
 
-	public void removeTile(){
-		this.tile = null;
-	}
-	
-	public char getCharacter() {
-		if(tile == null)
-			return ' ';
-		else
-			return tile.getLetter(); 
-	}
-	
-	public int getWordMultiplier() {
-		return type.getWordMultiplier();
-	}
-	
-	public int getLetterMultiplier() {
-		return type.getLetterMultiplier();
-	}
-	
-	public boolean isOccupied() {
-		return  this.tile == null ? false : true;
-	}
+    public boolean isTripleLetter() {
+        return letterMuliplier == 3;
+    }
 
-	public int getTileValue(){
-		return this.tile.getValue();
-	}
+    public boolean isDoubleWord() {
+        return wordMultiplier == 2;
+    }
 
-	public String toString() {
-		if(isEmpty())
-			return type.toString();
-		else
-			return "" + tile.toString();
-	}
+    public boolean isTripleWord() {
+        return wordMultiplier == 3;
+    }
 
-	public int getPlacementScore() {
+    public void add(Tile tile) {
+        isOccupied = true;
+        this.tile = tile;
+    }
 
-			int placementScore;
-			int lm;
-			int tv;
-			if (this.isEmpty())
-				return 0;
-			else {
-				lm=this.getLetterMultiplier();
-				tv= this.getTileValue();
-				placementScore = lm *tv;
-			}
-			return placementScore;
-		}
-	}
+    public boolean isOccupied() {
+        return isOccupied;
+    }
 
+    // getTile pre-condition: isOccupied must be true
+    public Tile getTile() {
+        return tile;
+    }
 
+}
