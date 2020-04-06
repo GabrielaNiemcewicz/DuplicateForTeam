e4cea53a9ccd4f75525f7d0a7c11f90589682034
GabrielaNiemcewicz <43764470+GabrielaNiemcewicz@users.noreply.github.com>
2020-04-06 09:27:08 +0100

    dictionary bookmarks as binary running out of time deadline

--- Dictionary.java
+++ Dictionary.java
@@ -5,7 +5,7 @@ public class Dictionary
 {
     Set<String> dictionary = new HashSet<>();
     public ArrayList<String> dictionar = new ArrayList<String>();
-    int [] bookmarks = new int [27];//26 letters and 1 blank
+    int [] bookmarks = new int [27];
     boolean match;
     String soughtWord;
 
@@ -15,25 +15,32 @@ public class Dictionary
         dictionar = new ArrayList<String>();
         bookmarks = new int [27];//26 letters and 1 blank
         match = false;
+        this.readToArrayList();
+        this.findbookmark();
 
     }
     void getWord(String soughtWord){
         this.soughtWord = soughtWord;
 
-    }
-
+    }//gimme a minute, I'll look at sth in ui
+//so, which do we have? we have the bineyseach you wrote for me i take pics and send it to you okay
     public ArrayList<String> getDictionar()
     {
         return dictionar;
     }
+
     public void findbookmark()
     { //pecondition: use after writing readtoarralist
         bookmarks[0] = 0;
         int bookIndex=1;
         for (int i =1; i<dictionar.size();i++)
             if (dictionar.get(i).charAt(0)!=dictionar.get(i-1).charAt(0))
-            {bookmarks[bookIndex] = i+1;
+            {
+                bookmarks[bookIndex] = i+1;
                 bookIndex++;}
+        bookmarks[bookIndex] = 267750;
+
+
     }
   /*  public boolean setInDictionary()
     {
@@ -43,7 +50,7 @@ public class Dictionary
     }*/
 
 
-    public void readToArrayList (Scrabble scrabble)
+    public void readToArrayList ()
     {
         BufferedReader objReader = null;
         try
@@ -55,7 +62,7 @@ public class Dictionary
                 System.out.println("file doesn't exist");
             while ((currentline = sin.readLine()) != null)
             {
-                scrabble.dictionar.add(currentline);
+                this.dictionar.add(currentline.trim().toLowerCase());
             }
         }
         catch (IOException e)
@@ -87,10 +94,12 @@ public class Dictionary
     {
         boolean isIn = dictionar.contains(this.soughtWord);
        return isIn;
-    }
-    //I'm still waiting for my phone to charge
+    } //i want to push dictionary to git, where is file?
+    //precondition:: we used getWord first
     public boolean dictionarysearch(String soughtWord)
-    {
+    { //*  int index = (int) soughtWord[0];
+      //*  int start = bookmarks[]*//
+
         for (int i = 0; i<dictionar.size(); i++) //that's simpler, linear search for now- but on ArrayList
             if(dictionar.get(i).equalsIgnoreCase(soughtWord)) //that's method where we can put any kind of search algorithm
             {
@@ -110,7 +119,7 @@ public class Dictionary
         this.dictionarysearch(soughtWord);
         return match ? false:true;
     }
-   /* public static void main (String[]args)
+    public static void main (String[]args)
     {
         Board board = new Board();
        Word big = new Word(8,8,true,"BIG");
@@ -118,7 +127,7 @@ public class Dictionary
        board.lastWord = big;
         System.out.println(board.lastWord.getLetters()+"word in board letters");
         Dictionary  dick = new Dictionary();
-        dick.readToArrayList();
+      //  dick.readToArrayList();
         System.out.println(dick.dictionar.get(101)+"is part of dict");
         //dick.dictionarysearch("witc"); is your phone on just starting to charge enough now
       //  if(dick.contain(board))
@@ -127,5 +136,7 @@ public class Dictionary
             System.out.println(board.lastWord.getLetters()+"not word");
         System.out.println(dick.dictionar.contains("big"));
 
-    }*/
+        dick.findbookmark();
+
+    }
 }
